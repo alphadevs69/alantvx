205753df4c3f8414c95bce00585ca253feb4d6f5
alphadev69 <alphaboraddevs@gmail.com>
2026-05-26 13:29:54 +0700

    Implement user-agent checks for request handling
    Added user-agent checks to identify browsers, downloaders, and players.

--- functions/[[playlist]].js
+++ functions/[[playlist]].js
@@ -34,13 +34,55 @@ const secFetchMode =
 const isBrowser =
   secFetchDest === "document" ||
   secFetchMode === "navigate";
+const ua =
+  request.headers.get("user-agent") || "";
 
+const lowerUA = ua.toLowerCase();
+
+// Downloader umum
+const isDownloader =
+  lowerUA.includes("idm") ||
+  lowerUA.includes("internet download manager") ||
+  lowerUA.includes("adm") ||
+  lowerUA.includes("advanced download manager") ||
+  lowerUA.includes("wget") ||
+  lowerUA.includes("curl") ||
+  lowerUA.includes("python") ||
+  lowerUA.includes("aria2");
+
+// OTT / Android player
+const isPlayer =
+  lowerUA.includes("ott") ||
+  lowerUA.includes("navigator") ||
+  lowerUA.includes("iptv") ||
+  lowerUA.includes("tivimate") ||
+  lowerUA.includes("vlc") ||
+  lowerUA.includes("kodi") ||
+  lowerUA.includes("exoplayer") ||
+  lowerUA.includes("exo") ||
+  lowerUA.includes("dalvik") ||
+  lowerUA.includes("lavf") ||
+  lowerUA.includes("okhttp");
 // Browser buka playlist → redirect homepage
-if (isBrowser) {
+// Browser asli → homepage
+if (isBrowser && !isPlayer) {
   return Response.redirect(
     "https://alantvid.pages.dev/",
     302
   );
+}
+  // Downloader block
+if (isDownloader) {
+  return new Response("Forbidden", {
+    status: 403,
+    headers: {
+      "Content-Type":
+        "text/plain; charset=utf-8",
+
+      "Cache-Control":
+        "no-store"
+    }
+  });
 }
   // playlist tidak ada
   if (!playlists[path]) {
